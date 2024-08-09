8811b1471d996946072a48a21929a7b37d28a33a
RaydenDarkus <svpatil20000@gmail.com>
2024-08-09 01:54:15 -0400

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,23 @@ import Projects from './components/Projects/Projects';
 import Contact from './components/Contact/Contact';
 import Footer from './components/Footer/Footer';
 import { Element } from 'react-scroll';
+import { motion, useScroll, useSpring } from "framer-motion";
 
 function App() {
+
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001
+  });
+
   return (
     <>
       <div id='navbar'>
         <Navbar/>
       </div>
+      <motion.div className="progress-bar" style={{ scaleX }} />
       <div className="container">
         <Element name="hero">
           <Hero/>
